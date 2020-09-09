29bda41bd1fdd3649d4629984dda8f6c4a5466c4
MusigwaOAF <pacifique.musigwa@oneacrefund.org>
2020-09-09 20:05:12 +0200

    Updates

---
+++ precache-manifest.bd5325f0e030741ec5e92d5c47ce7f65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e6af000c270ebf6cefca8f59271c885",
+    "url": "/Display/index.html"
+  },
+  {
+    "revision": "7e653b09efde520b3202",
+    "url": "/Display/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c96a869ec05a7aed11c4",
+    "url": "/Display/static/css/main.1ea8de92.chunk.css"
+  },
+  {
+    "revision": "7e653b09efde520b3202",
+    "url": "/Display/static/js/2.a4ef58af.chunk.js"
+  },
+  {
+    "revision": "8d1c7ff10238f9f79e61179e48274665",
+    "url": "/Display/static/js/2.a4ef58af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c96a869ec05a7aed11c4",
+    "url": "/Display/static/js/main.c7de886b.chunk.js"
+  },
+  {
+    "revision": "a6708d03ebec9d9ac20b",
+    "url": "/Display/static/js/runtime-main.5446213b.js"
+  },
+  {
+    "revision": "6371448ba5acba6d6501983155b67370",
+    "url": "/Display/static/media/cloud.6371448b.svg"
+  }
+]);
